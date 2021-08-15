2be558c8c56eef2d500472ae4899fbd93e3c3540
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-08-15 00:52:48 -0300

    programa

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const formEl = document.querySelector("form");
 const outputEl = document.querySelector("#output");
 const inputEl = document.querySelector("#input");
-const switchEl = document.querySelector(".switch");
 const title = document.querySelector(".title");
 const copy = document.querySelector(".copy");
 
@@ -13,25 +12,7 @@ formEl.addEventListener("submit", (e) => {
   convert(inputType, inputValue);
 });
 
-switchEl.addEventListener("click", (e) => {
-  const type = e.target.getAttribute("data-type");
-  inputEl.value = "";
-  outputEl.innerText = "";
 
-  if (type === "binary") {
-    e.target.setAttribute("data-type", "text");
-    inputEl.setAttribute("data-type", "text");
-    inputEl.setAttribute("placeholder", "Input Plain Text...");
-    outputEl.innerText = "Binary Output...";
-    title.innerText = "Text To Binary";
-  } else if (type === "text") {
-    e.target.setAttribute("data-type", "binary");
-    inputEl.setAttribute("data-type", "binary");
-    inputEl.setAttribute("placeholder", "Input Binary Code...");
-    outputEl.innerText = "Text Output...";
-    title.innerText = "Binary To Text";
-  }
-});
 
 function convert(type, inputValue) {
   let output = "";
